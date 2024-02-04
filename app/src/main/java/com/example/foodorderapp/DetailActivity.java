ed1dac5e52fba442bcc6981c55a0add23f7f206e
SiddhantGujrathi1 <gujrathisiddhant@gmail.com>
2024-02-04 17:57:13 +0530

    Intent Passing Added

--- app/src/main/java/com/example/foodorderapp/DetailActivity.java
+++ app/src/main/java/com/example/foodorderapp/DetailActivity.java
@@ -11,8 +11,21 @@ public class DetailActivity extends AppCompatActivity {
     ActivityDetailBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        binding = ActivityDetailBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+            super.onCreate(savedInstanceState);
+            binding = ActivityDetailBinding.inflate(getLayoutInflater());
+            setContentView(binding.getRoot());
+
+            int image = getIntent().getIntExtra("image",0);
+            int price = getIntent().getIntExtra("price",0);
+            String name = getIntent().getStringExtra("name");
+            String desc = getIntent().getStringExtra("desc");
+
+
+            binding.detailImage.setImageResource(image);
+            binding.detailItemName.setText(name);
+            binding.detailPrice.setText(String.format("%d",price));
+            binding.detailDescription.setText(desc);
+
+
     }
 }
